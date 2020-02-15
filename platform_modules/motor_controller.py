e74b0a2a85670d8a57a1e4b6214465926e88f409
Viet Anh Nguyen <vietanh@vietanhdev.com>
2020-02-15 22:49:44 +0700

    Fixbug motor controller

--- platform_modules/motor_controller.py
+++ platform_modules/motor_controller.py
@@ -19,7 +19,7 @@ class MotorController(threading.Thread):
         # Init controller
         self.pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)
         self.pwm.set_pwm_freq(cf.MOTOR_FREQ)
-        usleep(5000)
+        usleep(10000)
 
         # Reset state
         self.direction = 0
@@ -40,17 +40,16 @@ class MotorController(threading.Thread):
 
     def set_speed(self, throttle_val):
         if gs.emergency_stop:
-            self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, 0)
+            self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, cf.THROTTLE_NEUTRAL)
+            usleep(187500)
             return
-
         if throttle_val > 0:
             if self.direction == -1:
                 self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, cf.THROTTLE_MAX_FORWARD)
                 usleep(187500)
                 self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, cf.THROTTLE_NEUTRAL)
-                direction = 0
+                self.direction = 0
                 usleep(187500)
-
             self.direction = 1
             pwm = self.value_map(throttle_val, 0, 100, cf.THROTTLE_NEUTRAL, cf.THROTTLE_MAX_FORWARD)
             self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, pwm)
@@ -59,11 +58,13 @@ class MotorController(threading.Thread):
                 self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, cf.THROTTLE_MAX_REVERSE)
                 usleep(187500)
                 self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, cf.THROTTLE_NEUTRAL)
-                direction = 0
+                self.direction = 0
                 usleep(187500)
             self.direction = -1
             pwm = 4095 - self.value_map( abs(throttle_val), 0, 100 , 4095 - cf.THROTTLE_NEUTRAL , 4095 - cf.THROTTLE_MAX_REVERSE)
             self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, pwm)
+        else:
+            self.pwm.set_pwm(cf.THROTTLE_CHANNEL, 0, cf.THROTTLE_NEUTRAL)
         usleep(5000)
 
     def set_steer(self, steer_angle):
