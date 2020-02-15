318e527af564391c75ec32004165bc4fb88ba860
Viet Anh Nguyen <vietanh@vietanhdev.com>
2020-02-15 19:53:31 +0700

    Add code to determine presses buttons

--- test_lcd_buttons.py
+++ test_lcd_buttons.py
@@ -12,6 +12,14 @@ lcd = LCD(config.LCD_ADDRESS)
 button_reader = ButtonReader()
 button_reader.start()
 
+# Save last value of buttons
+last_button_1 = False
+last_button_2 = False
+last_button_3 = False
+last_button_4 = False
+last_button_ss1 = False
+last_button_ss2 = False
+
 while True:
     lcd.lcd_clear()
     lcd.lcd_display_string("UI testing", 1)
@@ -19,8 +27,34 @@ while True:
     button_values = [gs.button_1, gs.button_2, gs.button_3, gs.button_4, gs.button_ss1, gs.button_ss2]
     button_values = list(map(int, button_values)) 
     lcd.lcd_display_string("{} {} {} {} {} {}".format(*button_values), 3)
-    lcd.lcd_display_string(str(datetime.datetime.now().time()), 4)
-    time.sleep(2)
+    
+    # Determine pressed buttons
+    pressed_buttons = []
+    if gs.button_1 and not last_button_1:
+        pressed_buttons.append("1")
+    if gs.button_2 and not last_button_2:
+        pressed_buttons.append("2")
+    if gs.button_3 and not last_button_3:
+        pressed_buttons.append("3")
+    if gs.button_4 and not last_button_4:
+        pressed_buttons.append("4")
+    if gs.button_ss1 and not last_button_ss1:
+        pressed_buttons.append("S1")
+    if gs.button_ss2 and not last_button_ss2:
+        pressed_buttons.append("S2")
+
+    if pressed_buttons:
+        lcd.lcd_display_string("Pressed:" + ",".join(pressed_buttons), 4)
+
+    # Update values
+    last_button_1 = gs.button_1
+    last_button_2 = gs.button_2
+    last_button_3 = gs.button_3
+    last_button_4 = gs.button_4
+    last_button_ss1 = gs.button_ss1
+    last_button_ss2 = gs.button_ss2
+
+    time.sleep(1)
 
 
 button_reader.join()
