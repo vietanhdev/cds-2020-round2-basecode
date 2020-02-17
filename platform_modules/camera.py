201df3df72b5f669879a71586a39229b502156c5
Viet Anh Nguyen <vietanh@vietanhdev.com>
2020-02-17 21:56:00 +0700

    Write speed and steering with videos

--- platform_modules/camera.py
+++ platform_modules/camera.py
@@ -26,6 +26,7 @@ class Camera(threading.Thread):
         self.device = openni2.Device.open_any()
 
         self.record_videos = gs.record_videos
+        self.frame_count = 0
 
     def run(self):
 
@@ -37,14 +38,16 @@ class Camera(threading.Thread):
         depth_grabber = DepthCamera(self.device)
         depth_grabber.start()
 
+        # Create logger with 'camera'
+        self.logger = logging.getLogger('camera')
+        self.logger.setLevel(logging.INFO)
+
         while not gs.exit_signal:
-            time.sleep(1)
+            time.sleep(1/20.0)
             if self.record_videos != gs.record_videos:
                 if gs.record_videos: # Turn on video recording
 
                     print("Start recording...")
-
-                    gs.is_recording = False
 
                     folder_name = time.strftime("%Y%m%d-%H%M%S")
 
@@ -54,21 +57,39 @@ class Camera(threading.Thread):
                     gs.rgb_video_file = os.path.join(video_folder, "rgb_" + folder_name + ".avi")
                     gs.depth_video_file = os.path.join(video_folder, "depth_" + folder_name + ".avi")
 
-                    gs.rgb_frame_count = 0
+                    self.logger.handlers = []
+                    fh = logging.FileHandler(os.path.join(video_folder, "log.txt"))
+                    formatter = logging.Formatter('%(asctime)s %(message)s')
+                    fh.setFormatter(formatter)
+                    self.logger.addHandler(fh)
 
-                    gs.is_recording = True
+                    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+                    self.rgb_out_file = cv2.VideoWriter(gs.rgb_video_file, fourcc, 20.0, (320,240))
+                    self.depth_out_file = cv2.VideoWriter(gs.depth_video_file, fourcc, 20.0, (320, 240))
 
-                    rgb_grabber.start_recording(gs.rgb_video_file)
-                    depth_grabber.start_recording(gs.depth_video_file)
+                    self.frame_count = 0
 
                 else: # Stop video recording
 
                     print("Stop recording...")
 
-                    rgb_grabber.stop_recording()
-                    depth_grabber.stop_recording()
+                    self.rgb_out_file.release()
+                    self.depth_out_file.release()
 
                 self.record_videos = gs.record_videos
+
+            elif self.record_videos: # Write video to file
+
+                # Write rgb video
+                self.rgb_out_file.write(gs.rgb_frames.get())
+
+                # Write depth video
+                img_rgb = cv2.cvtColor(gs.depth_frames.get(),cv2.COLOR_GRAY2BGR)
+                cvuint8 = cv2.convertScaleAbs(img_rgb, alpha=(255.0/65535.0))
+                self.depth_out_file.write(cvuint8)
+
+                self.logger.info("FrameID=" + str(self.frame_count) + " Speed=" + str(gs.speed) + " Steer=" + str(gs.steer))
+                self.frame_count += 1
 
 
         # Wait threads to exit
@@ -80,24 +101,10 @@ class Camera(threading.Thread):
 
 class RGBCamera(threading.Thread):
 
-    out_file = None
-    is_recording = False
-
     def __init__(self, device):
         threading.Thread.__init__(self)
         self.device = device
-
     def run(self):
-
-        # create logger with 'rgb_camera'
-        logger = logging.getLogger('rgb_camera')
-        logger.setLevel(logging.DEBUG)
-        fh = logging.FileHandler('rgb_camera.log')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-
-        logger.debug("Start rgb image thread")
 
         # Start camera stream
         rgb_stream = self.device.create_color_stream()
@@ -110,42 +117,12 @@ class RGBCamera(threading.Thread):
             bgr   = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
             put_to_queue_no_wait_no_block(bgr, gs.rgb_frames)
-            
-            if RGBCamera.is_recording:
-                RGBCamera.out_file.write(bgr)
-
-
-        logger.debug("Exiting from RGB image grabber")
-
-    def start_recording(self, file_path):
-        if RGBCamera.out_file is not None:
-            self.stop_recording()
-        
-        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-        RGBCamera.out_file = cv2.VideoWriter(file_path, fourcc, 20.0, (320,240))
-
-        RGBCamera.is_recording = True
-
-    def stop_recording(self):
-        if not self.is_recording:
-            return
-        if RGBCamera.out_file is not None:
-            try:
-                RGBCamera.out_file.release()
-            except e:
-                print(e)
-        RGBCamera.is_recording = False
-
 
 class DepthCamera(threading.Thread):
-
-    out_file = None
-    is_recording = False
 
     def __init__(self, device):
         threading.Thread.__init__(self)
         self.device = device
-        self.out_file = None
     def run(self):
 
         print("Start depth image thread")
@@ -164,30 +141,5 @@ class DepthCamera(threading.Thread):
             img = cv2.flip(img,1)
 
             put_to_queue_no_wait_no_block(img, gs.depth_frames)
-            
-            if DepthCamera.is_recording:
-                img_rgb = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-                cvuint8 = cv2.convertScaleAbs(img_rgb, alpha=(255.0/65535.0))
-                DepthCamera.out_file.write(cvuint8)
-
 
         print("Exiting from Depth image grabber")
-
-    def start_recording(self, file_path):
-        if DepthCamera.is_recording:
-            self.stop_recording()
-        
-        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-        DepthCamera.out_file = cv2.VideoWriter(file_path, fourcc, 20.0, (320, 240))
-
-        DepthCamera.is_recording = True
-
-    def stop_recording(self):
-        if not self.is_recording:
-            return
-        if DepthCamera.is_recording:
-            try:
-                DepthCamera.out_file.release()
-            except e:
-                print(e)
-        DepthCamera.is_recording = False
